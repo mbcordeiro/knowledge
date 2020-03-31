cafe0ba0a52c5904ae5f589d73be2dbbb55c6be8
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-03-31 14:49:37 -0300

    Validate admin

--- frontend/src/config/router.js
+++ frontend/src/config/router.js
@@ -7,6 +7,8 @@ import ArticlesByCategory from '@/components/article/ArticlesByCategory'
 import ArticleById from '@/components/article/ArticleById'
 import Auth from '@/components/auth/Auth'
 
+import { userKey } from '@/global'
+
 Vue.use(VueRouter)
 
 const routes = [{
@@ -17,6 +19,7 @@ const routes = [{
     name: 'adminPages',
     path: '/admin',
     component: AdminPages,
+    meta: { requiresAdmin: true }
 }, {
     name: 'articlesByCategory',
     path: '/categories/:id/articles',
@@ -34,6 +37,17 @@ const routes = [{
 const router = new VueRouter({
     mode: 'history',
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    const json = localStorage.getItem(userKey)
+
+    if(to.matched.some(record => record.meta.requiresAdmin)) {
+        const user = JSON.parse(json)
+        user && user.admin ? next() : next({ path: '/' })
+    } else {
+        next()
+    }
 })
 
 export default router
